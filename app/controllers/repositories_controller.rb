b89da58e1552e9236adace52842e8b2176f466c4
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-01-21 15:05:15 -0800

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
   def index
   	github = GithubService.new({"access_token" => session[:token]})
-  	@repos_array = github.get_repos.to_json
+  	@repos_array = github.get_repos
   end
 
   def create
